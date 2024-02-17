e606b1f424ff3232a5a01a8abc02152c66f878b1
Xuxuan Lin <141507967+LinxxTHU@users.noreply.github.com>
2024-02-18 00:53:06 +0800

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,11 +1,11 @@
-const OSS_BASE_URL: string = 'https://wechatmsg-1316931797.cos.ap-beijing.myqcloud.com/'
+const OSS_BASE_URL: string = 'https://xxxxxxxxxxxx.cos.ap-xxxxx.myqcloud.com/'
 
-const API_BASE_URL_BUILD: string = 'https://chat.linxxthu.com'
+const API_BASE_URL_BUILD: string = 'https://api.xxxxxxxx.com'
 
 const API_BASE_URL_DEV: string = '/api'
 
-const PASSWORD_RAW_MD5: string = '34EC86C55BE7C802700DE80DFABCC4B9'
-const PASSWORD_UPPER_MD5: string = '14D4CE4276D23DEC8D4B29CBDBCC9455'
+const PASSWORD_RAW_MD5: string = 'XXXXXXXXXXXXXXXXXXXXXXX'
+const PASSWORD_UPPER_MD5: string = 'XXXXXXXXXXXXXXXXXXXXXXX'
 
 export {
     OSS_BASE_URL,
